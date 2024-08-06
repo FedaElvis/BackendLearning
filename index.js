5149c1f77b06c17d6fd84abb8a54e9886ebc99f7
FEDA_ELVIS <elvisfeda123@gmail.com>
2024-08-06 12:27:47 +0000

    learnt how to connect static pages to the server

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ import express from 'express'
 import middlewares from './middlewares/logs.js'
 const app = express();
 
-app.get("/", middlewares, (req,res,next)=>{
-    res.send("welcome midllewares")
-    next()
+app.use(express.static('./public'))
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(process.cwd()), "./public/index.html" )
 })
-
-
 
 app.listen(8000,()=>console.log("server up"))
